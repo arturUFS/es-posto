4c1a8fd132ce0baa6d0b82394f6866492bd80418
arturgds <arturgds053@gmail.com>
2025-03-18 14:22:58 -0300

    sequelize config e models exemplo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 const routes = require("./routes/index");
+//const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
@@ -12,17 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 app.use("/", routes);
 app.use("/assets", express.static("assets"));
 
 const PORT = process.env.PORT || 3000;
+
+const {database} = require("./config/database");
+const authDB = async () => {
+    try {
+        await database.authenticate();
+        console.log("Banco de dados sincronizado.");
+    } 
+    catch (error) {
+        console.error("Erro ao sincronizar DB:", error);
+    }
+};
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    authDB();
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
