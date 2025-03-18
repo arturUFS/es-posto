ec9e9dc49d170469eb9c1881dc05b8bb55b24565
arturgds <arturgds053@gmail.com>
2025-03-18 14:22:58 -0300

    sequelize config e models exemplo

---
+++ models/usuario.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import { database } from "../config/database.js";
+import { config } from "dotenv";
+
+config()
+export const Usuario = database.define(
+  "usuario",
+  {
+    usuario: {
+      type: DataTypes.STRING(50),
+      primaryKey: true,
+    },
+    senha: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    cpf: {
+      type: DataTypes.STRING(14),
+      references: {
+        model: "funcionario",
+        key: "cpf",
+      },
+    },
+  },
+  {
+    tableName: "usuario",
+    schema: "posto",
+    timestamps: false,
+  }
+);
+
+// Definir a relação com Funcionario
+Usuario.associate = (models) => {
+  Usuario.belongsTo(models.Funcionario, {
+    foreignKey: "cpf",
+    as: "funcionario",
+  });
+};
+
