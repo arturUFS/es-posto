0a62dd7aff3909b770b575777262e504dbe11c46
Lanna Luara <lannanovaes010@gmail.com>
2025-03-29 18:01:18 -0300

    Correçoes

--- controllers/servicoController.js
+++ controllers/servicoController.js
@@ -163,7 +163,7 @@ export const servicoController = {
       })
 
       //Registra a venda
-      const novaVenda = await Venda.create({
+      await Venda.create({
         codigo: idVenda,
         data: dataServico,
         valor: valor,
@@ -171,8 +171,6 @@ export const servicoController = {
         formapagamento: formaPagamento,
         idagendamento: idAgendamento,
       });
-
-      console.log(novaVenda);
 
       res.json({ message: "✅ Agendamento registrado com sucesso!" });
     } catch (error){
